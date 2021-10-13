ca33e878daa85a9b2d46fab5ccc4cfa83a1a4dba
walters-skn <mickywalters72@gmail.com>
2021-10-13 16:03:18 -0400

    Final js file

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 /* Add your JavaScript to this file */
-window.onload = function(){
-    var form = document.getElementsByClassName("form")
-    var message = document.getElementsByClassName("message")
-    // const errors = document.getElementById('errors')
-    
-    form.addEventListener('submit', function(e){
-        const email = document.getElementById("email").value
-        // let message =[]
-        
-        if(email == ""){
-            message.innerHTML("Email is required")
-        }
+
+document.addEventListener("DOMContentLoaded", function(){
+	
+	var email = document.getElementById("email");
+	var message = document.getElementsByClassName("message")[0];
+	var submit = document.getElementsByTagName("button")[0];
+	
+	submit.addEventListener("click", function(e){
+		e.preventDefault();
+		
+		if (email.value == ""){
+			message.innerHTML = "Email is required for submission"
+		}
         else{
-            message.innerHTML= "Thank You For Subscribing! Your email" + email + "has been submitted"
-        }
-        e.preventDefault()
-    })
-}
+			message.innerHTML = "Thank You For Subscribing! Your email" + email + "has been submitted";
+		}
+	});
+});
