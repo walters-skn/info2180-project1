aacae1cc4068b16ee0b55c50f1bb3065faa2e592
walters-skn <mickywalters72@gmail.com>
2021-10-13 16:02:12 -0400

    Added js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var form = document.getElementsByClassName("form")
+    var message = document.getElementsByClassName("message")
+    // const errors = document.getElementById('errors')
+    
+    form.addEventListener('submit', function(e){
+        const email = document.getElementById("email").value
+        // let message =[]
+        
+        if(email == ""){
+            message.innerHTML("Email is required")
+        }
+        else{
+            message.innerHTML= "Thank You For Subscribing! Your email" + email + "has been submitted"
+        }
+        e.preventDefault()
+    })
+}
